d477dffd08237f420ea06a3428d70d8e418daba6
deekshithmd <deekshithmogra@gmail.com>
2023-05-26 23:24:27 +0530

    initial setup

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const container = document.querySelector(".container");
+const fruits = [
+  { name: "apple", image: "images/apple.jpg" },
+  { name: "pinapple", image: "images/pinapple.jpg" },
+  { name: "kiwi", image: "images/kiwi.jpg" },
+  { name: "mango", image: "images/mango.jpg" },
+  { name: "strawberry", image: "images/strawberry.jpg" },
+];
+
+const showFruits = () => {
+  let output = "";
+  fruits.forEach(
+    ({ name, image }) =>
+      (output += `
+    <div class="card">
+      <img class="card--avatar" src=${image} />
+      <h1 class="card--title">${name}</h1>
+      <a class="card--link" href="#">Taste</a>
+    </div>
+    `)
+  );
+  container.innerHTML = output;
+};
+
+document.addEventListener("DOMContentLoaded", showFruits);
